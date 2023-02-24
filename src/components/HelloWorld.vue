151e974010e75b786e5d45d87fc4f18f667a9fc8
aote777 <1540464777@qq.com>
2023-02-24 15:16:48 +0800

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,10 @@
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+      <a
+        href="https://cli.vuejs.org"
+        target="_blank"
+        rel="noopener"
         >vue-cli documentation</a
       >.
     </p>
@@ -54,36 +57,63 @@
     <h3>Essential Links</h3>
     <ul>
       <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+          rel="noopener"
+          >Core Docs</a
+        >
       </li>
       <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+          rel="noopener"
           >Forum</a
         >
       </li>
       <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+          rel="noopener"
           >Community Chat</a
         >
       </li>
       <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+          rel="noopener"
           >Twitter</a
         >
       </li>
       <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+        <a
+          href="https://news.vuejs.org"
+          target="_blank"
+          rel="noopener"
+          >News</a
+        >
       </li>
     </ul>
     <h3>Ecosystem</h3>
     <ul>
       <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+        <a
+          href="https://router.vuejs.org"
+          target="_blank"
+          rel="noopener"
           >vue-router</a
         >
       </li>
       <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+        <a
+          href="https://vuex.vuejs.org"
+          target="_blank"
+          rel="noopener"
+          >vuex</a
+        >
       </li>
       <li>
         <a
@@ -94,7 +124,10 @@
         >
       </li>
       <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+        <a
+          href="https://vue-loader.vuejs.org"
+          target="_blank"
+          rel="noopener"
           >vue-loader</a
         >
       </li>
@@ -111,30 +144,30 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+  import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-});
+  export default defineComponent({
+    name: 'HelloWorld',
+    props: {
+      msg: String,
+    },
+  })
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
