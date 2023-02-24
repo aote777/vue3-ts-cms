39e370f216ee1f2e08778676f014916513af2b1a
aote777 <1540464777@qq.com>
2023-02-24 17:03:53 +0800

    fix

--- src/service/index.ts
+++ src/service/index.ts
@@ -2,6 +2,20 @@ import Request from './request'
 
 const request = new Request({
   baseURL: process.env.VUE_APP_BASE_URL,
+  interceptors: {
+    requestInterceptor(config) {
+      return config
+    },
+    requestInterceptorCatch(err) {
+      return err
+    },
+    responseInterceptor(config) {
+      return config
+    },
+    responseInterceptorCatch(err) {
+      return err
+    },
+  },
 })
 
 export default request
--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,9 +1,61 @@
-import axios from 'axios'
-import type { AxiosInstance, CreateAxiosDefaults } from 'axios'
+import axios, { AxiosResponse } from 'axios'
+import type {
+  AxiosInstance,
+  CreateAxiosDefaults,
+  AxiosRequestConfig,
+} from 'axios'
+
+interface RequestInterceptor {
+  requestInterceptor?: (config: any) => any
+  requestInterceptorCatch?: (err: any) => any
+  responseInterceptor?: (config: AxiosResponse) => AxiosResponse
+  responseInterceptorCatch?: (err: any) => any
+}
+
+interface configInterceptor extends CreateAxiosDefaults {
+  interceptors?: RequestInterceptor
+}
+
 class Request {
   instance: AxiosInstance
-  constructor(config?: CreateAxiosDefaults) {
+  interceptors?: RequestInterceptor
+  constructor(config?: configInterceptor) {
     this.instance = axios.create(config)
+    this.interceptors = config?.interceptors
+
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptor,
+      this.interceptors?.requestInterceptorCatch
+    )
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptor,
+      this.interceptors?.responseInterceptorCatch
+    )
+
+    // 添加所有实例都有的拦截器
+    this.instance.interceptors.request.use(
+      (config) => {
+        return config
+      },
+      (err) => {
+        return err
+      }
+    )
+
+    this.instance.interceptors.response.use(
+      (response) => {
+        return response
+      },
+      (err) => {
+        return err
+      }
+    )
+  }
+
+  request(config: AxiosRequestConfig) {
+    this.instance.request(config).then((res) => {
+      console.log(res)
+    })
   }
 }
 
